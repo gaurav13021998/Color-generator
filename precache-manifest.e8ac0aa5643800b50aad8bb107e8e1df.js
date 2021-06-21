905bcb4b95a4e153c222a8a25da0babc74b22f74
Gaurav Singh <gaurav13021997@gmail.com>
2021-06-21 09:52:26 +0530

    Updates

---
+++ precache-manifest.e8ac0aa5643800b50aad8bb107e8e1df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cba9a494346eacc174d63d67794e1bec",
+    "url": "/color-generator/index.html"
+  },
+  {
+    "revision": "18af0d1d54ff31b9a390",
+    "url": "/color-generator/static/css/main.b9f22eb8.chunk.css"
+  },
+  {
+    "revision": "cf6bca8e816ecf72b2d4",
+    "url": "/color-generator/static/js/2.11f691c1.chunk.js"
+  },
+  {
+    "revision": "1dbab337b90e8f2eb0a4a79d2378f49b",
+    "url": "/color-generator/static/js/2.11f691c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18af0d1d54ff31b9a390",
+    "url": "/color-generator/static/js/main.73d58deb.chunk.js"
+  },
+  {
+    "revision": "ed92ef5652ce8f57a79c",
+    "url": "/color-generator/static/js/runtime-main.c4c3dd28.js"
+  }
+]);
